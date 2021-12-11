d87fca038f588c59a57f2706492f46bb9b757e6a
Jiapei Zeng <448311217@qq.com>
2021-12-11 21:41:50 +0800

    Over in 'Blueprints and Views' step

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,4 +32,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # import and register the 'auth' blueprint from the factory using 'app.register_blueprint()'
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
